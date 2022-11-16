7f605af8321c00499ee20b9c0f899176bf5cba1e
Lucas de Bona Sartor <lucas.bsartor@pagar.me>
2022-11-16 01:30:14 -0300

    Added Dockerfile

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -11,7 +11,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   if (!token) {
     return {
       redirect: {
-        destination: '/login',
+        destination: '/auth/login',
         permanent: false,
       },
     }
